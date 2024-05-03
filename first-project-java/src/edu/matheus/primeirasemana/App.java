36aa978e658a0256dee094dc126dcad268157f4f
matheus <mthoffmann1@hotmail.com>
2024-05-03 16:25:38 -0300

    pedrapapeltesoura

---
+++ first-project-java/src/edu/matheus/primeirasemana/App.java
@@ -0,0 +1,16 @@
+package edu.matheus.primeirasemana;
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Pedra, Papel, Tesoura!!!");
+        System.out.println("O que você joga?");
+        String opcao = System.in.read();
+
+        System.exit(0);
+    }
+
+    public static String jogo (String resultado); {
+        return resultado; 
+    }
+
+
+}
